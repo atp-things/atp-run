b47eb37b36a2e76ca7fc123be227f21893edfd9b
andrazpolak <andrazpolak@gmail.com>
2025-09-01 21:21:59 +0200

    done

--- src/atprun/main.py
+++ src/atprun/main.py
@@ -1,19 +1,24 @@
+# https://typer.tiangolo.com/tutorial/package/
+
 import subprocess
 from pprint import pprint
 
 import typer
 from atptools import DictDefault
 
-default_config_path: str = "./.atprun.yml"
+default_config_path: str = "./atprun.yml"
 app = typer.Typer()
+config: DictDefault = DictDefault()
 
 
 @app.callback()
 def callback():
     """
-    Awesome Portal Gun
+    Awesome Portal Gun 2
     """
-    typer.echo("Shooting Call back.")
+    # load config file
+    config.from_file(path=default_config_path)
+    pass
 
 
 @app.command()
@@ -21,7 +26,35 @@ def script(name: str):
     """
     Run script
     """
-    typer.echo(f"Run script '{name}'")
+
+    # get script config
+    scripts: DictDefault | None = config.get("scripts")
+    if scripts is None:
+        typer.secho(
+            "Error: No scripts entity in config found",
+            err=True,
+            fg=typer.colors.RED,
+        )
+        return
+
+    if name not in scripts:
+        typer.secho(
+            f"Error: Script '{name}' not found",
+            err=True,
+            fg=typer.colors.RED,
+        )
+        return
+
+    script: DictDefault = scripts[name]
+
+    # execute command
+    command: str = script["run"]
+    if len(command) <= 0:
+        raise ValueError("No command found for script")
+    subprocess.run(
+        args=command,
+        shell=True,
+    )
 
 
 if __name__ == "__main__":
